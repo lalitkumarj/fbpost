52f087243ef7cb7cdc41e39e0af78d63a2796004
jain <lalitkumarj@gmail.com>
2013-12-31 10:39:23 -0600

    Basic db working

--- example.py
+++ example.py
@@ -38,10 +38,12 @@ import urllib2
 import urllib
 import json
 import random
+import collections
 
 from google.appengine.ext import blobstore
 from google.appengine.ext.webapp import blobstore_handlers
 from google.appengine.ext import db
+from google.appengine.ext import ndb
 from google.appengine.api import images
 from webapp2_extras import sessions
 
@@ -50,15 +52,22 @@ config['webapp2_extras.sessions'] = dict(secret_key='')
 #upload_url_rpc = blobstore.create_upload_url_async('/upload')
 #upload_url = upload_url_rpc.get_result()
 
-class User(db.Model):
-    id = db.StringProperty(required=True)
-    created = db.DateTimeProperty(auto_now_add=True)
-    updated = db.DateTimeProperty(auto_now=True)
-    name = db.StringProperty(required=True)
-    profile_url = db.StringProperty(required=True)
-    access_token = db.StringProperty(required=True)
+class User(ndb.Model):
+    id = ndb.StringProperty(required=True)
+    created = ndb.DateTimeProperty(auto_now_add=True)
+    updated = ndb.DateTimeProperty(auto_now=True)
+    name = ndb.StringProperty(required=True)
+    profile_url = ndb.StringProperty(required=True)
+    access_token = ndb.StringProperty(required=True)
 
-
+class Post(ndb.Model):
+    page_post_url = ndb.StringProperty(required=True)
+    post_url = ndb.StringProperty(required=True)
+    picture_url = ndb.StringProperty(required=True)
+    container = ndb.StringProperty(required=True)
+    date = ndb.DateTimeProperty(auto_now_add=True)
+    status = ndb.StringProperty(required=True)
+    
 class BaseHandler(webapp2.RequestHandler):
     """Provides access to the active Facebook user in self.current_user
 
@@ -81,7 +90,9 @@ class BaseHandler(webapp2.RequestHandler):
             if cookie:
                 # Okay so user logged in.
                 # Now, check to see if existing user
-                user = User.get_by_key_name(cookie["uid"])
+                #user = User.get_by_key_name(cookie["uid"])
+                user = User.get_by_id(cookie["uid"])
+                
                 if not user:
                     # Not an existing user so get user info
                     graph = facebook.GraphAPI(cookie["access_token"])
@@ -145,31 +156,22 @@ fac
 class HomeHandler(BaseHandler):
     def get(self):        
         if self.current_user != None:
-            self.redirect("/service")
+            self.redirect("/portal")
         else:
             self.redirect("/login")
 
 class LoginHandler(BaseHandler):
     def get(self):
         if self.current_user:
-            self.redirect("/service")
+            self.redirect("/portal")
         else:
             template = jinja_environment.get_template('login.html')
             self.response.out.write(template.render(dict(
                         facebook_app_id=FACEBOOK_APP_ID,
                         current_user=self.current_user
                         )))
-
-class EndHandler(BaseHandler):
-    def get(self):
-        if self.current_user != None:
-            template = jinja_environment.get_template('end.html')
-            self.response.out.write(template.render(dict(
-                    facebook_app_id=FACEBOOK_APP_ID)))
-        else:
-            self.redirect("/")
         
-class FormHandler(BaseHandler):
+class ServiceHandler(BaseHandler):
     def get(self):
         if self.current_user != None: 
             template = jinja_environment.get_template('form.html')
@@ -190,53 +192,107 @@ class FormHandler(BaseHandler):
     def post(self):
         description = self.request.get('description_input')
         
-        select_pages = self.request.get('select_pages',allow_multiple=True)
+        select_pages = self.request.get('select_pages', allow_multiple=True)
         select_groups = self.request.get('select_groups',allow_multiple=True)
         
         blob_key = self.request.get('imagekey')
         blob_reader = blobstore.BlobReader(blob_key,buffer_size=1048576)
 
         pages = self.graph.get_connections("me","accounts")['data']
-        pages_dict = dict([(page['id'], page['access_token']) for page in pages])
-        
+        page_tokens = dict([(page['id'], page['access_token']) for page in pages])
+        page_names = dict([(page['id'], page['name']) for page in pages])
+
         groups = self.graph.get_connections("me","groups")['data']
-        #groups_dict = dict([(group['id'], group['access_token']) for group in groups])
-        #images.ImagesService imagesService = ImagesServiceFactory.getImagesService();
-
-#Which page_id to take? Need to reject more then one selection client side? Radio box?
-        for page_id in select_pages:
-            print page
-            print pages_dict[page_id]
-            posted_id =  self.graph.put_photo(blob_reader, description, album_id=page_id, access_token=pages_dict[page_id])['id']
-           #page_id = self.graph.put_wall_post(description, att{"picture":"facebook.com/10100739842477767"}, page)['id']
-        for group in select_groups:
-            #print images.get_serving_url(blob_key)
-            self.graph.put_wall_post(description, {"link":"http://facebook.com/%s"%(posted_id),"picture":images.get_serving_url(blob_key)}, group)
-            
-            self.redirect("/login")
+        group_names = dict([(group['id'], group['name']) for group in groups])
         
+#TODO: You should only get one page!
+        for page_id in select_pages:
+            page_posted_id =  self.graph.put_photo(blob_reader, description, album_id=page_id, access_token=page_tokens[page_id])['id']
+            # post = Post(parent=ndb.Key("User", self.current_user['id']),
+            #             post_url=posted_id, 
+            #             picture_url=images.get_serving_url(blob_key),
+            #             container = page_names[page_id]) 
+            # post.put()
+        
+        for group_id in select_groups:
+            posted_id =  self.graph.put_photo(blob_reader, description, album_id=group_id)['id']
+            post = Post(parent=ndb.Key("User", self.current_user['id']),
+                        post_url=posted_id, 
+                        page_post_url = page_posted_id,
+                        picture_url=images.get_serving_url(blob_key),
+                        container = page_names[page_id],
+                        status = "active") 
+            post.put()
+        
+        self.redirect('/redirect?submit_post')
 
-class ImageHandler(blobstore_handlers.BlobstoreUploadHandler):        
+
+class ManageHandler(BaseHandler):
+    def get(self):
+        if self.current_user != None: 
+            template = jinja_environment.get_template('manage.html')
+            query = Post.query(ancestor=ndb.Key("User",self.current_user['id']))
+            print query
+            
+#need a list of posts. get this list of posts by sorting by page_id. a post should be a dictionary . has a group field that has a list of 
+            all_posts = [dict([('post_url',"http://facebook.com/"+q.post_url), 
+                           ('date', q.date), 
+                           ('container', q.container),
+                           ('picture_url', q.picture_url)]) for q in query.iter()]
+            # posts = [dict([('post_url',"http://facebook.com/"+q.post_url), 
+            #                ('date', q.date), 
+            #                ('container', q.container),
+            #                ('picture_url', q.picture_url)]) for q in query.iter()]
+            
+            self.response.out.write(template.render(dict(
+                        facebook_app_id=FACEBOOK_APP_ID,
+                        current_user=self.current_user,
+                        posts = posts
+                        )))
+        else:
+            self.redirect("/")
+
+        
+class UploadHandler(blobstore_handlers.BlobstoreUploadHandler):        
     def post(self):
         upload_files = self.get_uploads('files[]')
         blob_info = upload_files[0]
         obj = {
-            'key': str(blob_info.key()) 
+            'upload_image_key': str(blob_info.key()) 
             } 
-        print "make a key"+obj['key']
-        self.redirect("/upload-redirect?"+urllib.urlencode(obj))
+        self.redirect("/redirect?"+urllib.urlencode(obj))
         
 
 class RedirectHandler(BaseHandler):    
     def get(self):
-        obj = {
-            'key': self.request.get('key')
-            } 
-        print "make a key"+obj['key']
-        self.response.headers['Content-Type'] = 'application/json'   
-        self.response.out.write(json.dumps(obj))
+        if self.request.get('upload_image_key') != '': 
+            obj = {
+                'key': self.request.get('upload_image_key')
+                } 
+            print "make a key"+obj['key']
+            self.response.headers['Content-Type'] = 'application/json'   
+            self.response.out.write(json.dumps(obj))
+        elif self.request.get('submit_post') != '':
+            self.redirect('/end')
 
+class PortalHandler(BaseHandler):
+    def get(self):
+        if self.current_user != None:
+            template = jinja_environment.get_template('portal.html')
+            self.response.out.write(template.render(dict(
+                    facebook_app_id=FACEBOOK_APP_ID)))
+        else:
+            self.redirect("/")
 
+class EndHandler(BaseHandler):
+    def get(self):
+        if self.current_user != None:
+            template = jinja_environment.get_template('end.html')
+            self.response.out.write(template.render(dict(
+                    facebook_app_id=FACEBOOK_APP_ID,
+                    current_user=self.current_user)))
+        else:
+            self.redirect("/")
 
 class LogoutHandler(BaseHandler):
     def get(self):
@@ -248,9 +304,11 @@ jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname("templates/base.html")) , autoescape=True, extensions=['jinja2.ext.autoescape'])
 
 application = webapp2.WSGIApplication(
-    [('/service', FormHandler),('/', HomeHandler), ('/login', LoginHandler), ('/logout', LogoutHandler), ('/service/post',FormHandler),('/upload',ImageHandler), ('/upload-redirect?',RedirectHandler), ('/end',EndHandler)],
+    [('/', HomeHandler), ('/login', LoginHandler), ('/portal', PortalHandler) , ('/manage', ManageHandler), ('/upload',UploadHandler), ('/service', ServiceHandler),('/service/post',ServiceHandler), ('/redirect?',RedirectHandler), ('/logout', LogoutHandler), ('/end',EndHandler)],
     debug=True,
     config=config
 )
 
 
+        # for group in select_groups:
+        #     self.graph.put_wall_post(description, {"link":"http://facebook.com/%s"%(posted_id),"picture":images.get_serving_url(blob_key)}, group)
